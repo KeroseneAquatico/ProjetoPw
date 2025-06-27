9e14fdffdeba246ad9f9a8aa1e4a4cb5a43c41c1
KeroseneAquatico <cleitonferraoalvares@gmail.com>
2025-06-27 20:39:07 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,15 @@
+const LoginEmail = document.querySelector("#LoginEmail");
+const LoginSenha = document.querySelector("#LoginSenha");
+const EsqueciSenha = document.querySelector("#EsqueciSenha");
+const Entrar = document.querySelector("#Entrar");
+
+const usuariosRegistro = localStorage.getItem('usuarios');
+const usuarios = JSON.parse(usuariosRegistro);
+
+
+
+Entrar.addEventListener("click", () => {
+
+
+
+})
