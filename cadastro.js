6c8db2fb4580607330fc6ad1bd182db8eb233069
KeroseneAquatico <cleitonferraoalvares@gmail.com>
2025-06-29 01:20:08 -0300

    Cadastro Inicial javascript

---
+++ cadastro.js
@@ -0,0 +1,67 @@
+const nomeCadastro=document.querySelector("#nomeCadastro");
+const EmailCadastro=document.querySelector("#emailCadastro");
+const senhaCadastro=document.querySelector("#senhaCadastro");
+const senhaCadastro2=document.querySelector("#senhaCadastro2");
+
+//Inputs de option
+const PlanoBasico=document.querySelector("#planoBasico");
+const PlanoPadrao=document.querySelector("#planoPadrao");
+const PlanoPremium=document.querySelector("#planoPremium");
+
+const CadastrarBtn=document.querySelector("#CadastrarBtn");
+const CadastroErro_msg=document.querySelector("#CadastroErro_msg");
+
+const armazenamentoLocal = localStorage.getItem('Users');
+
+let users=[];
+if(armazenamentoLocal){
+    let users = JSON.parse(armazenamentoLocal);
+}
+
+CadastrarBtn.addEventListener("click", () => {
+  if( EmailCadastro.value=="" || senhaCadastro.value=="" || senhaCadastro2.value=="" || nomeCadastro.value=="" ){
+ CadastroErro_msg.innerHTML="Preencha TODOS os campos!";   
+    
+}else if(senhaCadastro2.value!=senhaCadastro.value){
+
+    CadastroErro_msg.innerHTML="As senhas não conferem, por favor as insira novamente a senha";
+    senhaCadastro2.value="";
+    senhaCadastro.value ="";
+ }else{
+    let PlanoSelecionado;
+    if (PlanoBasico==true){
+     PlanoSelecionado=PlanoBasico.value;
+
+    }else if(PlanoPadrao==true){
+
+     PlanoSelecionado=PlanoPadrao.value;
+
+    }else{
+     
+    PlanoSelecionado=PlanoPremium.value;
+    }
+
+        //Planobasico.value==True.
+        let user={
+            perfil:[{
+                nomePerfil: nomeCadastro.value,
+                imagemPerfil: "https://i.pinimg.com/736x/a8/da/22/a8da222be70a71e7858bf752065d5cc3.jpg",
+                assistidoRecente:[],
+                assistirMaisTarde:[],
+            }],
+            email: EmailCadastro.value,
+            senha: senhaCadastro.value,
+            planoUser:PlanoSelecionado,
+            nome: nomeCadastro.value,
+    }
+
+     nomeCadastro.value="";
+     EmailCadastro.value="";
+     senhaCadastro.value="";
+     senhaCadastro2.value="";
+
+    users.push(user);
+    localStorage.setItem('Users',users);
+    window.location.href="login.html";
+    }    
+})
