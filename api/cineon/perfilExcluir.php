64be2df30b68ac2f4aad8eae9c1b9f3cdf0b1b37
KeroseneAquatico <cleitonferraoalvares@gmail.com>
2025-12-01 18:09:46 -0300

    aaa

---
+++ api/cineon/perfilExcluir.php
@@ -0,0 +1,13 @@
+<?php
+
+include '../connection.php';
+include '../session.php';
+
+$perfilId = $_GET['perfilId'];
+$userId = $_SESSION['user_id'];
+
+$stmt = $conn->prepare("DELETE FROM perfis WHERE id = ? AND user_id = ?");
+$stmt -> execute([$perfilId, $userId]);
+
+echo json_encode(['status' => 'success']);
+?>
