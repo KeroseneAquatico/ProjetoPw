66d0aee69b682201463612dc2f9c5ffcd7a48518
KeroseneAquatico <cleitonferraoalvares@gmail.com>
2025-12-01 00:46:48 -0300

    Cadastro perfil

--- api/auth/cadastro.php
+++ api/auth/cadastro.php
@@ -8,7 +8,8 @@ $password = $_POST["senhaCadastro"];
 $confirmPassword = $_POST["senhaCadastro2"];
 $planoAssinatura = $_POST["planoAssinatura"];
 
-if(!isset($name) ||
+if (
+    !isset($name) ||
     !isset($email) ||
     !isset($password) ||
     !isset($confirmPassword) ||
@@ -16,33 +17,34 @@ if(!isset($name) ||
     empty($name) ||
     empty($email) ||
     empty($password) ||
-    empty($confirmPassword) || 
+    empty($confirmPassword) ||
     empty($planoAssinatura)
-){
+) {
     echo json_encode([
         "error" => true,
         "message" => "Preencha TODOS os campos, por favor!"
     ]);
     exit;
-};
+}
+;
 
-if($password !== $confirmPassword){
+if ($password !== $confirmPassword) {
     echo json_encode([
         "error" => true,
         "message" => "As senhas não coincidem!"
     ]);
     exit;
 }
-if(strlen($password) < 8){
+if (strlen($password) < 8) {
     echo json_encode([
         "error" => true,
         "message" => "A senha deve ter no mínimo 8 caracteres!"
     ]);
     exit;
 }
-if($planoAssinatura == "basico"){
+if ($planoAssinatura == "basico") {
     $planoAssinatura = 1;
-} elseif($planoAssinatura == "padrão"){
+} elseif ($planoAssinatura == "padrão") {
     $planoAssinatura = 2;
 } else {
     $planoAssinatura = 3;
@@ -51,7 +53,9 @@ if($planoAssinatura == "basico"){
 $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 
 $stmt = $conn->prepare("INSERT INTO usuarios (nome, email, password, plano_id) VALUES (?, ?, ?, ?)");
-if($stmt->execute([ $name, $email, $hashedPassword, $planoAssinatura])){
+if ($stmt->execute([$name, $email, $hashedPassword, $planoAssinatura])) {
+    $stmtPerfil = $conn->prepare("INSERT INTO perfis (usuario_id, nome) VALUES (LAST_INSERT_ID(), ?)");
+    $stmtPerfil->execute([$name]);
     echo json_encode([
         "error" => false,
         "message" => "Cadastro realizado com sucesso!"
@@ -59,6 +63,6 @@ if($stmt->execute([ $name, $email, $hashedPassword, $planoAssinatura])){
 } else {
     echo json_encode([
         "error" => true,
-        "message" => "Erro ao cadastrar usuário: " 
+        "message" => "Erro ao cadastrar usuário: "
     ]);
 }
