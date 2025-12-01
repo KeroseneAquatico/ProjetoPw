37098cd9afe5319645fb302313630e2b3ce6ffbd
KeroseneAquatico <cleitonferraoalvares@gmail.com>
2025-12-01 18:44:41 -0300

    Progresso no bghl de criar prerfil

--- api/cineon/criarPerfil.php
+++ api/cineon/criarPerfil.php
@@ -3,10 +3,17 @@
 include '../connection.php';
 include '../session.php';
 
+$userId = $_SESSION['user_id'];
 $nomePerfil = $_POST('nomePerfil');
-$
+$iconePerfil = $_POST('iconePerfil');
 if( !isset($nomePerfil) || empty($nomePerfil)){
     echo json_encode(['error' => true, 'message' => 'Nome do perfil é obrigatório.']);
     exit;
 }
-if 
+if (!isset($iconePerfil) || empty($iconePerfil)){
+    echo json_encode(['error' => true, 'message' => 'Ícone do perfil é obrigatório.']);
+    exit;
+}
+
+$stmt = $conn->prepare("INSERT INTO perfis (user_id, nome, imagem) VALUES (?, ?, ?)");
+$stmt-> execute([$userId, $nomePerfil, $iconePerfil]);
